6daf976bb3e5d467fda317e8898461931194a077
Cory Huebner <chuebner86@gmail.com>
2020-07-06 19:18:08 -0500

    committing page

--- say_hello.py
+++ say_hello.py
@@ -1,14 +1,81 @@
 import argparse
 
 def say_hello(name):
-    if name in ['jaxon', 'levi', 'jax', 'levi james']:
-        print(f'you are the coolest kid {name}')
-    elif name == 'mom' or name == 'dad':
-        print(f'you are not even kids')
-    else:
-        print(f'i don\'t even know you {name}')
-
+    if name in ['arther','dingo']:
+        print(f'you are the funnest {name}')
+    elif name == 'dad' or name == 'levi':
+        print(f'your not so fun {name}')
+    
 parser = argparse.ArgumentParser(description='Say hello to people')
 parser.add_argument('name', help='The name of the person you want to say hello to')
 args = parser.parse_args()
-say_hello(args.name)
+say_hello(args.name) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
